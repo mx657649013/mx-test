8ae31e81ce62a3cfb2e977bd105928b8b208b7fb
maoxin419 <maoxin419@163.com>
2015-12-07 00:38:58 +0800

    ReadWriteLock

--- src/test/java/test/TestTest.java
+++ src/test/java/test/TestTest.java
@@ -1,9 +1,13 @@
 package test;
 
+import java.util.Random;
+
 public class TestTest {
 
 	public static void main(String[] args) {
 		System.out.println("test!!!");
+		System.out.println(Math.random() * 1000 + 2000);
+		System.out.println(new Random().nextInt(1000)+ 3000);
 	}
 
 }
